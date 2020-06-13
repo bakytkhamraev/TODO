008126c768eb553676bb015556dd7dafa57f0189
bakytkhamraev <khamraevbakyt@gmail.com>
2020-06-13 21:43:47 +0600

    Link

--- app/src/main/java/com/example/todo/MainActivity.java
+++ app/src/main/java/com/example/todo/MainActivity.java
@@ -18,7 +18,6 @@ public class MainActivity extends AppCompatActivity {
         - В IntroActivity две кнопки next и skip при клике на next перелистывать страницу ViewPager-a,
           на последнем экране ViewPager-a кнопку skip прятать и заменять текст кнопки next на start,
           кнопка start запускает MainActivity (при свайпе на предыдущие страницы состояние кнопок восстанавливать)
-        - В IntroActivity кнопка skip для старта MainActivity
-        */
+        - В IntroActivity кнопка skip для старта MainActivity*/
     }
 }
